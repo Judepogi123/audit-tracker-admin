417384b9e2ea50cd25e7f196c3b7300bdfd9a3a3
DILG Programmer <juderibleza36@gmail.com>
2024-05-20 16:35:54 +0800

    dasdas

--- server/api/axios.tsx
+++ server/api/axios.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
 const dev = "http://localhost:3000"
 export default axios.create({
-    baseURL: "https://judepogi123.github.io/Audit-tracker-test"
+    baseURL: "https://judepogi123.github.io/audit-tracker"
 })
